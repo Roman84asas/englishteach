e242c5f0659deddae461611ce593f90d2c44e61f
RomanMereneanu <asasin84@mail.ru>
2019-08-05 23:44:29 +0300

    Work with Content

--- templain-home.php
+++ templain-home.php
@@ -13,8 +13,8 @@ get_header();
     <section class="info_sect">
         <div class="wrapper">
             <div class="info_data font_nav">
-                <h2>Репетитор по английскому</h2>
-                <p class="description">Обучение английскому, немецкому языку с основ и до свободного владения. Помощь в подготовке к ЕГЭ и бакалавриату.</p>
+                <h2><?php the_field('info_labe'); ?></h2>
+                <p class="description"><?php the_field('descr_info'); ?></p>
                 <a href="#box_subscribe" class="button" id="button"><span>Узнать больше</span></a>
             </div>
 
@@ -24,88 +24,131 @@ get_header();
     <section class="services_icon">
         <div class="item">
             <div class="image_holder">
-                <img src="./assets/img/icon1.png" alt="">
+                <?php
+                $image = get_field('img_icon_1');
+                if ($image){ ?>
+                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                <?php }?>
             </div>
-            <h3>Английский с нуля</h3>
-            <p>Обучение английскому языку с нуля и до необходимого Вам уровня. Подход к каждому обучаемому индивидуален.</p>
+            <h3><?php the_field('icon_1'); ?></h3>
+            <p><?php the_field('info_icon_1'); ?></p>
         </div>
 
         <div class="item">
             <div class="image_holder">
-                <img src="assets/img/icon2.png" alt="">
+                <?php
+                $image = get_field('img_icon_2');
+                if ($image){ ?>
+                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                <?php }?>
             </div>
-            <h3>Подготовка к экзаменам</h3>
-            <p>Поэтапная подготовка к ЕГЭ, бакалавриату с учетом требований школьной или лицейской программы.</p>
+            <h3><?php the_field('icon_2'); ?></h3>
+            <p><?php the_field('info_icon_2'); ?></p>
         </div>
 
         <div class="item">
             <div class="image_holder">
-                <img src="assets/img/icon3.png" alt="">
+                <?php
+                $image = get_field('img_icon_3');
+                if ($image){ ?>
+                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                <?php }?>
             </div>
-            <h3>Разговорный английский</h3>
-            <p>Обучение разговорному ангийскому языку как для повседневного общения, так и для деловых разговоров.</p>
+            <h3><?php the_field('icon_3'); ?></h3>
+            <p><?php the_field('info_icon_3'); ?></p>
         </div>
 
         <div class="item">
             <div class="image_holder">
-                <img src="assets/img/icon4.png" alt="">
+                <?php
+                $image = get_field('img_icon_4');
+                if ($image){ ?>
+                    <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                <?php }?>
             </div>
-            <h3>IELTS, TOEFL, Cambridge ESOL</h3>
-            <p>В зависимости от необходимого Вам результата, подбирается индивидуальная программа к каждому тесту.</p>
+            <h3><?php the_field('icon_4'); ?></h3>
+            <p><?php the_field('info_icon_4'); ?></p>
         </div>
     </section>
 
     <section class="about_posts" id="about_posts">
         <div class="wrapper">
-            <h2 class="block_title font_nav">Обо мне</h2>
+            <h2 class="block_title font_nav"><?php the_field('title_about'); ?></h2>
 
             <div class="about_list">
                 <div class="about_item">
-                    <img src="assets/img/about1.jpg" alt="">
+                    <?php
+                    $image = get_field('about_img_1');
+                    if ($image){ ?>
+                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                    <?php }?>
                     <div class="data_line">
-                        <span class="about_img"><img src="assets/img/finger.png" alt=""></span>
+                        <span class="about_img">
+                            <?php
+                            $image = get_field('finger_img');
+                            if ($image){ ?>
+                                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                            <?php }?>
                         <span class="line"></span>
                     </div>
-                    <h3 class="about_data font_nav">
-Более 10 лет опыта работы
-</h3>
-
+                    <h3 class="about_data font_nav"><?php the_field('about_info_1'); ?></h3>
                 </div>
 
                 <div class="about_item">
-                    <img src="assets/img/about2.jpg" alt="">
+                    <?php
+                    $image = get_field('about_img_2');
+                    if ($image){ ?>
+                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                    <?php }?>
                     <div class="data_line">
-                        <span class="about_img"><img src="assets/img/finger.png" alt=""></span>
+                        <span class="about_img">
+                            <?php
+                            $image = get_field('finger_img');
+                            if ($image){ ?>
+                                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                            <?php }?>
+                        </span>
                         <span class="line"></span>
                     </div>
-                    <h3 class="about_data font_nav">
-Свободное владение пятью языками
-</h3>
-
+                    <h3 class="about_data font_nav"><?php the_field('about_info_2'); ?></h3>
                 </div>
 
                 <div class="about_item">
-                    <img src="assets/img/about3.jpg" alt="">
+                    <?php
+                    $image = get_field('about_img_3');
+                    if ($image){ ?>
+                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                    <?php }?>
                     <div class="data_line">
-                        <span class="about_img"><img src="assets/img/finger.png" alt=""></span>
+                        <span class="about_img">
+                            <?php
+                            $image = get_field('finger_img');
+                            if ($image){ ?>
+                                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                            <?php }?>
+                        </span>
                         <span class="line"></span>
                     </div>
-                    <h3 class="about_data font_nav">
-Опыт работы с разными возрастными группами
-</h3>
-
+                    <h3 class="about_data font_nav"><?php the_field('about_info_3'); ?></h3>
                 </div>
 
                 <div class="about_item">
-                    <img src="assets/img/about4.jpg" alt="">
+                    <?php
+                    $image = get_field('about_img_4');
+                    if ($image){ ?>
+                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                    <?php }?>
                     <div class="data_line">
-                        <span class="about_img"><img src="assets/img/finger.png" alt=""></span>
+                        <span class="about_img">
+                            <?php
+                            $image = get_field('finger_img');
+                            if ($image){ ?>
+                                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                            <?php }?>
+                        </span>
                         <span class="line"></span>
                     </div>
-                    <h3 class="about_data font_nav">
-Гибкий график занятий
-</h3>
-
+                    <h3 class="about_data font_nav"><?php the_field('about_info_4'); ?></h3>
                 </div>
             </div>
 
@@ -115,76 +158,95 @@ get_header();
     <section class="services_list" id="services_list">
         <div class="wrapper">
 
-            <h2 class="block_title font_nav">Сервис услуг</h2>
-            <p class="sub_title font_nav">Перечень условий проведения занятий, способов проведения и целей.</p>
+            <h2 class="block_title font_nav"><?php the_field('serv_title'); ?></h2>
+            <p class="sub_title font_nav"><?php the_field('serv_title_descr'); ?></p>
 
             <div class="product_list cf">
 
                 <div class="product">
                     <div class="image_holder">
-                        <img src="assets/img/service_img1.png" alt="">
+                        <?php
+                        $image = get_field('service_img_1');
+                        if ($image){ ?>
+                            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                        <?php }?>
                     </div>
 
                     <div class="data_holder">
-                        <span class="catig">Что Вам необходимо</span>
-                        <h3>Иностранный язык</h3>
-                        <p>Обучение ангийскому и немецкому языку по индивидуальной прграмме и в соответствии с Вашими целями.</p>
+                        <span class="catig"><?php the_field('service_catig_1'); ?></span>
+                        <h3><?php the_field('service_title_1'); ?></h3>
+                        <p><?php the_field('service_descr_1'); ?></p>
+                    </div>
+                </div>
+
+                <div class="product">
+                    <div class="image_holder">
+                        <?php
+                        $image = get_field('service_img_2');
+                        if ($image){ ?>
+                            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                        <?php }?>
+                    </div>
+
+                    <div class="data_holder">
+                        <span class="catig"><?php the_field('service_catig_2'); ?></span>
+                        <h3><?php the_field('service_title_2'); ?></h3>
+                        <p><?php the_field('service_descr_2'); ?></p>
+                    </div>
+                </div>
+
+                <div class="product">
+                    <div class="image_holder">
+                        <?php
+                        $image = get_field('service_img_3');
+                        if ($image){ ?>
+                            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                        <?php }?>
+                    </div>
+
+                    <div class="data_holder">
+                        <span class="catig"><?php the_field('service_catig_3'); ?></span>
+                        <h3><?php the_field('service_title_3'); ?></h3>
+                        <p><?php the_field('service_descr_3'); ?></p>
 
                     </div>
                 </div>
 
                 <div class="product">
                     <div class="image_holder">
-                        <img src="assets/img/service_img2.png" alt="">
+                        <?php
+                        $image = get_field('service_img_4');
+                        if ($image){ ?>
+                            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                        <?php }?>
                     </div>
 
                     <div class="data_holder">
-                        <span class="catig">Индивидуальный подход</span>
-                        <h3>Подход к преподaванию</h3>
-                        <p>В зависимости от ситуации, как преподавание на дому, так и выезд по месту жительства обучаемого.</p>
+                        <span class="catig"><?php the_field('service_catig_4'); ?></span>
+                        <h3><?php the_field('service_title_4'); ?></h3>
+                        <p><?php the_field('service_descr_4'); ?></p>
                     </div>
                 </div>
 
                 <div class="product">
                     <div class="image_holder">
-                        <img src="assets/img/service_img3.png" alt="">
+                        <?php
+                        $image = get_field('service_img_5');
+                        if ($image){ ?>
+                            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+                        <?php }?>
                     </div>
 
                     <div class="data_holder">
-                        <span class="catig">Колличествo</span>
-                        <h3>Возможно группой</h3>
-                        <p>Обучение ангийскому и немецкому языку возможно вести не только индивидуально с обучаемым, но и группой.</p>
-
-                    </div>
-                </div>
-
-                <div class="product">
-                    <div class="image_holder">
-                        <img src="assets/img/service_img4.png" alt="">
-                    </div>
-
-                    <div class="data_holder">
-                        <span class="catig">График</span>
-                        <h3>Как Вам удобно</h3>
-                        <p>Обучение осуществляется на основе гибкого графика и с учетом свободного времени клиента.</p>
-                    </div>
-                </div>
-
-                <div class="product">
-                    <div class="image_holder">
-                        <img src="assets/img/service_img5.png" alt="">
-                    </div>
-
-                    <div class="data_holder">
-                        <span class="catig">Законно</span>
-                        <h3>Официальная деятельность</h3>
-                        <p>Обучение языкам осуществляется законно, в соответствии с требованиями Республиканского патента на преподование.</p>
+                        <span class="catig"><?php the_field('service_catig_5'); ?></span>
+                        <h3><?php the_field('service_title_5'); ?></h3>
+                        <p><?php the_field('service_descr_5'); ?></p>
                     </div>
                 </div>
             </div>
 
             <div class="all_products">
-                <a href="#" class="font_nav">Все услуги</a>
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="font_nav">Все услуги</a>
             </div>
         </div>
     </section>
@@ -214,17 +276,25 @@ get_header();
 
     <section class="slider_info" id="slider_info">
         <div class="left_box">
-            <img src="assets/img/english.jpg" alt="">
+            <?php
+            $image = get_field('slider_img_1');
+            if ($image){ ?>
+                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+            <?php }?>
         </div>
         <div class="right_box">
             <div class="data_holder">
-                <span class="category">Языки для всех</span>
-                <h3 class="font_nav">Английский и немецкий языки</h3>
-                <p class="description">Обучениу с нуля и до необходимого уровня. Подготовка к бакалавриату или экзаменам.</p>
+                <span class="category"><?php the_field('slider_info_1'); ?></span>
+                <h3 class="font_nav"><?php the_field('slider_title_1'); ?></h3>
+                <p class="description"><?php the_field('slider_descr_1'); ?></p>
             </div>
         </div>
         <div class="product_img">
-            <img src="assets/img/teach.jpg" alt="">
+            <?php
+            $image = get_field('slider_img_all');
+            if ($image){ ?>
+                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+            <?php }?>
         </div>
     </section>
 
